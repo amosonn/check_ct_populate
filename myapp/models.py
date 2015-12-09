6c6139b53433bbad88fcf7eb9c02c7c5ac297b63
Amos Onn <amosonn@gmail.com>
2015-12-09 15:43:33 +0200

    First state of app.
    * Wrapper has GenericForeignKey.
    * First is empty.

---
+++ myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Wrapper(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField(db_index=True)
+    wrapped = GenericForeignKey('content_type', 'object_id')
+
+
+class First(models.Model):
+    pass
